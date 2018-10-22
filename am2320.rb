3d07d8c72d9b282364ea4359a5501b87177530d7
yuki-iwamoto <yuki_iwamoto@genesis-healthcare.jp>
2018-10-22 12:17:35 +0900

    initial commit

---
+++ am2320.rb
@@ -0,0 +1,77 @@
+require 'rubygems'
+require 'i2c'
+require 'pry'
+
+class AM2320
+
+  def initialize(path, address = 0x5c)
+    @device = I2C.create(path)
+    @address = address
+  end
+
+	def tempature_standard_deviation(tempAry)
+		actualDiffTempature = 2.6;
+		#average tempature
+		sumTempature = tempAry.inject(0){|result, n|result + n}
+		averageTempature = if tempAry.size
+			sumTempature / tempAry.size
+		else
+			actualDiffTempature
+		end
+		# TODO func henkan
+		#targetTempature= averageTempature - actualDiffTempature
+		targetTempature = averageTempature
+		return targetTempature.round(1)
+	end
+
+  def read
+    #センサーを付ける
+    begin
+      @device.write(@address, "")
+    rescue
+      #TODO いつか例外でした際の処理を決めてやる必要があり
+      return nil
+    end
+    #センサー情報読み込み
+    begin
+      #x03:最低気温,x00:最高湿度,x04 情報保有
+      s = @device.read(@address, 8, "\x03\x00\x04")
+    rescue
+      #TODO いつか例外でした際の処理を決めてやる必要があり
+      return nil
+    end
+    #温度センサーで値が取れなかった場合、処理のやり直し
+    if s.nil?
+      return nil
+    end
+    # TODO ここの使わない変数は温度取得時に必要なのか？
+    func_code, ret_len, hum_h, hum_l, temp_h, temp_l, crc_l, crc_h = s.bytes.to_a
+    temp = (temp_h << 8) | temp_l
+    temp = temp / 10.0
+    #温度センサーで異常値を取得した場合に弾く
+    if temp <= 0.0 || 100.0 <= temp
+      return nil
+    end
+    return temp
+  end
+end
+
+# process start
+sensor = AM2320.new('/dev/i2c-1')
+
+tempAry = []
+# 処理を終えるかどうかフラグで管理する
+reloopFlg = true
+while reloopFlg do
+  temp = sensor.read
+  if temp != nil
+    tempAry << temp
+    if tempAry.size == 5
+      reloopFlg = false
+    else
+      sleep(1)
+    end
+  end
+end
+
+targetTempature = sensor.tempature_standard_deviation(tempAry)
---
+++ spread_sheet.rb
@@ -0,0 +1,94 @@
+require './am2320'
+require 'google_drive'
+require 'date'
+require 'csv'
+require 'pry'
+require 'fileutils'
+
+# process start
+sensor = AM2320.new('/dev/i2c-1')
+
+# pre process before measure tempature
+#30.times {
+#  sensor.read
+#}
+
+# measure tempature process
+tempAry = []
+# 処理を終えるかどうかフラグで管理する
+#reloopFlg = true
+end_time = Time.now + 20
+# ５回分の温度を取得する
+while Time.now < end_time do
+  temp = sensor.read
+  tempAry << temp unless temp.nil?
+  sleep(1)
+end
+# get average tempature
+targetTempature = sensor.tempature_standard_deviation(tempAry)
+#csv
+time = Time.new()
+Ymd = time.strftime('%Y%m%d')
+Hi = time.strftime('%H:%M')
+new_csv = "tempature#{Ymd}.csv"
+#CSV中身読み込み
+rows = []
+newCsvCreateFlg = false
+unless File.exist?("/home/pi/#{new_csv}")
+ newCsvCreateFlg = true;
+end
+#begin
+#  rows= CSV.read(new_csv)
+#  CSV.close_read(new_csv)
+#rescue
+#  newCsvCreateFlg = true
+#end
+#binding.pry
+#rows = []
+#FileUtils.chmod(0755, new_csv)
+if newCsvCreateFlg
+  # 今日の分新規作成
+	CSV.open(new_csv,'w') do |rows|
+	 rows << ["#{Ymd}"]
+	 rows << ["#{Hi}"]
+	 rows << ["#{targetTempature.to_s}"]
+	end
+else
+  # 今日の分追記更新
+	CSV.open(new_csv,'a') do |rows|
+	 rows << ["#{Ymd}"]
+	 rows << ["#{Hi}"]
+	 rows << ["#{targetTempature.to_s}"]
+	end
+end
+
+#Google spreadsheet
+sheet_id = "1bmR08oGhP01-DOLhlvuF_foJGIQEguXwL9-pD6R38ho"
+json_file = "/home/pi/Test temperature Server room-c61a8093e3eb.json"
+
+options = JSON.parse(File.read(json_file))
+key = OpenSSL::PKey::RSA.new(options['private_key'])
+
+auth = Signet::OAuth2::Client.new(
+  token_credential_uri: options['token_uri'],
+  audience: options['token_uri'],
+  scope: %w(
+    https://www.googleapis.com/auth/drive
+    https://docs.google.com/feeds/
+    https://docs.googleusercontent.com/
+    https://spreadsheets.google.com/feeds/
+  ),
+  issuer: options['client_email'],
+  signing_key: key
+)
+auth.fetch_access_token!
+
+# スプレッドシートの取得
+session = GoogleDrive.login_with_oauth(auth.access_token)
+ws = session.spreadsheet_by_key(sheet_id).worksheets[0]
+# ヘッダー後の列に追記する
+latest_row = ws.num_rows + 1
+ws[latest_row,1] = time.strftime("%Y/%m/%d %H:%M")
+ws[latest_row,2] = targetTempature.to_s
+
+ws.save
